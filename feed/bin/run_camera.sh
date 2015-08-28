46c68d5ed9aa5cabf25c102a79146cc7b939e02f
Gavin Cabbage <gavincabbage@gmail.com>
2015-08-28 13:46:30 -0400

    test page and changed feed threads to 1

--- feed/bin/run_camera.sh
+++ feed/bin/run_camera.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo feed/venv/bin/gunicorn --timeout 3600 -w 2 -b 0.0.0.0:9090 --error-logfile - --access-logfile - feed:app
+sudo feed/venv/bin/gunicorn --timeout 3600 -w 1 -b 0.0.0.0:9090 --error-logfile - --access-logfile - feed:app
