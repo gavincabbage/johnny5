a5d7ffd57a60e96d217cde38f0458aa3978cdd1f
Gavin Cabbage <gavincabbage@gmail.com>
2015-08-29 22:00:40 -0400

    removed panic and return 500 in case of i2c write failure

--- core/handlers.go
+++ core/handlers.go
@@ -11,40 +11,46 @@ func lookHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("enter lookHandler")
 	direction := mux.Vars(r)["direction"]
 	err := bot.Look(direction)
+    statusCode := 200
 	if err != nil {
-		w.WriteHeader(500)
-		panic(err)
+		statusCode = 500
 	}
-	w.WriteHeader(200)
+	w.WriteHeader(statusCode)
 }
 
 func moveHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("enter moveHandler")
 	direction := mux.Vars(r)["direction"]
 	err := bot.Move(direction)
+    statusCode := 200
 	if err != nil {
-		w.WriteHeader(500)
-		panic(err)
+		statusCode = 500
 	}
-	w.WriteHeader(200)
+	w.WriteHeader(statusCode)
 }
 
 func ledHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("enter ledHandler")
 	color := mux.Vars(r)["color"]
-
 	var err error
 	if r.Method == "POST" {
 		err = bot.LedOn(color)
 	} else if r.Method == "DELETE" {
 		err = bot.LedOff(color)
 	}
-
+    statusCode := 200
 	if err != nil {
-		w.WriteHeader(500)
-		panic(err)
+		statusCode = 500
 	}
-	w.WriteHeader(200)
+	w.WriteHeader(statusCode)
+}
+
+func distanceHandler(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(204)
+}
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(204)
 }
 
 func errorHandler() {
